1f433a5d1e1b2a93c61135502921068edb3f2880
Alexander Ewetumo <trinoxf@gmail.com>
2015-01-20 16:51:14 +0100

    moving bits around

--- grid.js
+++ grid.js
@@ -131,6 +131,7 @@ var Plug = exports.Plug = stacks.Configurable.extends({
     this.newIn('in');
     this.newOut('out');
     this.newOut('err');
+
     this.ignoreFilter.off();
     this.channelStore.hookBinderProxy(this);
 
@@ -373,157 +374,156 @@ var Rack = exports.Rack = stacks.Configurable.extends({
 });
 
 var Network = exports.Network = stacks.Configurable.extends({
-  init: function(id,rs,fn){
-    stacks.Asserted(stacks.valids.isString(id),'a string must be supplied as network id');
-    if(stacks.valids.exists(rs) && stacks.valids.not.Function(rs)){
-      stacks.Asserted(RackSpace.isInstance(rs),'if you must supply a non function as second please supply a rackspace instance');
-    }
-    this.$super();
-    this.id = id;
-    this.rs = rs;
-    this.plugs = stacks.Storage.make();
-    this.makeName = this.$bind(function(sn){
-      if(stacks.valids.not.String(sn)){ return; }
-      return [this.id,sn].join('.');
-    });
-    this.packetBlock = stacks.Proxy(this.$bind(function(d,n,e){
-      if(stacks.StreamPackets.isPacket(d)){
-        d.traces.push(this.GUUID);
+    init: function(id,rs,fn){
+      stacks.Asserted(stacks.valids.isString(id),'a string must be supplied as network id');
+      if(stacks.valids.exists(rs) && stacks.valids.not.Function(rs)){
+        stacks.Asserted(RackSpace.isInstance(rs),'if you must supply a non function as second please supply a rackspace instance');
       }
-      return n();
-    }));
-
-    this.channelStore = stacks.ChannelStore.make(this.id);
-    this.store = this.$bind(function(){ return this.channelStore; });
-    var plug;
-    var self = this;
-
-    this.pub('detachAll');
-    this.pub('detachAllIn');
-    this.pub('detachAllOut');
-    this.pub('releaseOut');
-    this.pub('releaseIn');
-    this.pub('networkAttached');
-    this.pub('networkDetached');
-
-    this.newIn = this.$bind(function(id,tag,picker){
-      return this.channelStore.newIn((id),tag || "*",picker)(this.$bind(function(tk){
-          tk.mutate(this.packetBlock.proxy);
-          tk.Packets = stacks.StreamPackets.proxy(function(){
-            tk.emit(this);
-          });
+      this.$super();
+      this.id = id;
+      this.rs = rs;
+      this.plugs = stacks.Storage.make();
+      this.makeName = this.$bind(function(sn){
+        if(stacks.valids.not.String(sn)){ return; }
+        return [this.id,sn].join('.');
+      });
+      this.packetBlock = stacks.Proxy(this.$bind(function(d,n,e){
+        if(stacks.StreamPackets.isPacket(d)){
+          d.traces.push(this.GUUID);
+        }
+        return n();
       }));
-    });
 
-    this.newOut = this.$bind(function(id,tag,picker){
-      return this.channelStore.newOut((id),tag || "*",picker)(this.$bind(function(tk){
-          tk.mutate(this.packetBlock.proxy);
-          tk.Packets = stacks.StreamPackets.proxy(function(){
-            tk.emit(this);
-          });
-      }));
-    });
+      this.channelStore = stacks.ChannelStore.make(this.id);
+      this.store = this.$bind(function(){ return this.channelStore; });
+      var plug;
+      var self = this;
 
-    this.$secure('toPlug',function(){
-      if(Plug.instanceBelongs(plug)) return splug;
-      plug = Plug.make(this.id,{ id: this.id });
-      plug.attachNetwork(this);
-      plug.withNetwork(this.in(),this.out());
-      return plug;
-    });
+      this.pub('detachAll');
+      this.pub('detachAllIn');
+      this.pub('detachAllOut');
+      this.pub('releaseOut');
+      this.pub('releaseIn');
+      this.pub('networkAttached');
+      this.pub('networkDetached');
 
-    this.$secure('imprint',function(net){
-      if(!Network.instanceBelongs(net)) return;
-      return fn.call(net);
-    });
+      this.newIn = this.$bind(function(id,tag,picker){
+        return this.channelStore.newIn((id),tag || "*",picker)(this.$bind(function(tk){
+            tk.mutate(this.packetBlock.proxy);
+            tk.Packets = stacks.StreamPackets.proxy(function(){
+              tk.emit(this);
+            });
+        }));
+      });
 
-    this.newIn('in');
-    this.newOut('out');
-    this.newOut('err');
+      this.newOut = this.$bind(function(id,tag,picker){
+        return this.channelStore.newOut((id),tag || "*",picker)(this.$bind(function(tk){
+            tk.mutate(this.packetBlock.proxy);
+            tk.Packets = stacks.StreamPackets.proxy(function(){
+              tk.emit(this);
+            });
+        }));
+      });
 
-    this.channelStore.hookBinderProxy(this);
+      this.$secure('toPlug',function(){
+        if(Plug.instanceBelongs(plug)) return splug;
+        plug = Plug.make(this.id,{ id: this.id });
+        plug.attachNetwork(this);
+        plug.withNetwork(this.in(),this.out());
+        return plug;
+      });
 
-    this.$dot(rs || fn);
-  },
-  use: function(plug,gid){
-    stacks.Asserted(Plug.instanceBelongs(plug),'first argument is required to be a plug instance');
-    if(!this.plugs.has(gid || plug.GUUID)){
-      this.plugs.add(gid || plug.GUUID,plug);
-      plug.gid = gid;
-      // plug.attachPlate(this.plate,this);
-    }
-    return this;
-  },
-  get: function(gid){
-    stacks.Asserted(stacks.valids.isString(gid),'argument is the unique alias for this plug');
-    return this.plugs.Q(gid);
-  },
-  removePlug: function(gid){
-    if(!this.has(gid)) return;
-    var pl = this.get(gid);
-    pl.release();
-    return pl;
-  },
-  destroyPlug: function(gid){
-    var f = this.remove(gid);
-    if(f) f.close();
-    return f;
-  },
-  hasPlug: function(id){
-    return this.plugs.has(id);
-  },
-  in: function(f){
-    if(stacks.valids.not.String(f)) f = 'in';
-    return this.channelStore.in(f);
-  },
-  out: function(f){
-    if(stacks.valids.not.String(f)) f = 'out';
-    return this.channelStore.out(f);
-  },
-  hasIn: function(f){
-    if(stacks.valids.not.String(f)) return;
-    return this.channelStore.hasIn(f);
-  },
-  hasOut: function(f){
-    if(stacks.valids.not.String(f)) return;
-    return this.channelStore.hasOut(f);
-  },
-  releaseOut: function(xchan){
-    if(stacks.valids.String(xchan) && !this.hasOut(xchan)) return;
-    var xc = this.out(xchan);
-    xc.unbindAll();
-    this.emit('release',xchan);
-  },
-  releaseIn: function(xchan){
-    if(stacks.valids.String(xchan) && !this.hasIn(xchan)) return;
-    var xc = this.out(xchan);
-    xc.unbindAll();
-    this.emit('release',xchan);
-  },
-  detachAll: function(){
-    this.store().unbindAllIn();
-    this.store().unbindAllOut();
-    this.emit('detachAll');
-  },
-  detachAllOut: function(){
-    this.store().unbindAllOut();
-    this.emit('detachAllOut');
-  },
-  detachAllIn: function(){
-    this.store().unbindAllIn();
-    this.emit('detachAllOut');
-  },
-},{
-  blueprint: function(fx,nt){
-    var print =  stacks.funcs.curry(Network.make,fx);
-    print.imprint = stacks.funcs.bind(function(net){
-      if(!Network.instanceBelongs(net)) return;
-      var res = fx.call(net);
-      return stacks.valids.exists(res) ? res : net;
-    },print)
-    return print;
-  },
-}).muxin({
+      this.$secure('imprint',function(net){
+        if(!Network.instanceBelongs(net)) return;
+        return fn.call(net);
+      });
+
+      this.newIn('in');
+      this.newOut('out');
+      this.newOut('err');
+
+      this.channelStore.hookBinderProxy(this);
+
+      this.$dot(rs || fn);
+    },
+    use: function(plug,gid){
+      stacks.Asserted(Plug.instanceBelongs(plug),'first argument is required to be a plug instance');
+      if(!this.plugs.has(gid || plug.GUUID)){
+        this.plugs.add(gid || plug.GUUID,plug);
+        plug.gid = gid;
+      }
+      return this;
+    },
+    get: function(gid){
+      stacks.Asserted(stacks.valids.isString(gid),'argument is the unique alias for this plug');
+      return this.plugs.Q(gid);
+    },
+    removePlug: function(gid){
+      if(!this.has(gid)) return;
+      var pl = this.get(gid);
+      pl.release();
+      return pl;
+    },
+    destroyPlug: function(gid){
+      var f = this.remove(gid);
+      if(f) f.close();
+      return f;
+    },
+    hasPlug: function(id){
+      return this.plugs.has(id);
+    },
+    in: function(f){
+      if(stacks.valids.not.String(f)) f = 'in';
+      return this.channelStore.in(f);
+    },
+    out: function(f){
+      if(stacks.valids.not.String(f)) f = 'out';
+      return this.channelStore.out(f);
+    },
+    hasIn: function(f){
+      if(stacks.valids.not.String(f)) return;
+      return this.channelStore.hasIn(f);
+    },
+    hasOut: function(f){
+      if(stacks.valids.not.String(f)) return;
+      return this.channelStore.hasOut(f);
+    },
+    releaseOut: function(xchan){
+      if(stacks.valids.String(xchan) && !this.hasOut(xchan)) return;
+      var xc = this.out(xchan);
+      xc.unbindAll();
+      this.emit('release',xchan);
+    },
+    releaseIn: function(xchan){
+      if(stacks.valids.String(xchan) && !this.hasIn(xchan)) return;
+      var xc = this.out(xchan);
+      xc.unbindAll();
+      this.emit('release',xchan);
+    },
+    detachAll: function(){
+      this.store().unbindAllIn();
+      this.store().unbindAllOut();
+      this.emit('detachAll');
+    },
+    detachAllOut: function(){
+      this.store().unbindAllOut();
+      this.emit('detachAllOut');
+    },
+    detachAllIn: function(){
+      this.store().unbindAllIn();
+      this.emit('detachAllOut');
+    },
+  },{
+    blueprint: function(fx,nt){
+      var print =  stacks.funcs.curry(Network.make,fx);
+      print.imprint = stacks.funcs.bind(function(net){
+        if(!Network.instanceBelongs(net)) return;
+        var res = fx.call(net);
+        return stacks.valids.exists(res) ? res : net;
+      },print)
+      return print;
+    },
+  }).muxin({
   a: function(net,chan,xchan){
     if(!Network.instanceBelongs(net)) return;
     if(stacks.valids.String(chan) && !plug.store().hasIn(chan)) return;
